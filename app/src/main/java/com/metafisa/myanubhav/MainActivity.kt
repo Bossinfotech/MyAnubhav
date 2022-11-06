e2a3c408b8a7e865133ede9cfcabc6151110f011
Boss <bossinfotech007@gmail.com>
2022-11-06 10:13:17 +0530

    Searchview Option bugs fixed , homefragments added in while opening in Application
    Rewards, discount_bg.xml changed ,

--- app/src/main/java/com/metafisa/myanubhav/MainActivity.kt
+++ app/src/main/java/com/metafisa/myanubhav/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.metafisa.myanubhav
 
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,10 +7,7 @@ import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.metafisa.myanubhav.fragments.ExploreFragment
-import com.metafisa.myanubhav.fragments.HomeFragment
-import com.metafisa.myanubhav.fragments.RewardFragment
-import com.metafisa.myanubhav.fragments.UserProfileFragment
+import com.metafisa.myanubhav.fragments.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,12 +16,12 @@ class MainActivity : AppCompatActivity() {
     private val homeFragment = HomeFragment()
     private val rewardFragment = RewardFragment()
     private val userProfileFragment=UserProfileFragment()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //anubhav
+       replaceFragmentmain(homeFragment)
+
         val bottomNavinationView =findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         bottomNavinationView.setOnItemSelectedListener {
@@ -41,27 +37,27 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
-
     }
 
     private fun replaceActivity() {
-        //newpro
         supportActionBar?.hide()
-
-            val view: View =layoutInflater.inflate(R.layout.bottom_sheet,null)
-            val dialog= BottomSheetDialog(this)
-            dialog.setContentView(view)
-            dialog.show()
-
+        val view: View =layoutInflater.inflate(R.layout.bottom_sheet,null)
+        val dialog= BottomSheetDialog(this)
+        dialog.setContentView(view)
+        dialog.show()
     }
 
-    //anubhav
     private fun replaceFragment(fragment: Fragment)
     {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.container,fragment)
         transaction.commit()
-
-
     }
+    private fun replaceFragmentmain(fragment: Fragment)
+    {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
 }
